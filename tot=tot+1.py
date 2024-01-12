e34b140da4598d62efee671f1df4a76bb8852c6a
Jandro331 <156459588+Jandro331@users.noreply.github.com>
2024-01-12 13:23:50 -0500

    Update tot=tot+1.py

---
+++ tot=tot+1.py
@@ -0,0 +1,12 @@
+#variable tot=0
+tot = 0
+#definite loop(execute an exact number of times)
+#for i in the secuence (5,4,3,2,1)
+for i in [5, 4, 3, 2, 1] :
+#we dont have the variable i in the loop
+#so we only repeat the loop 5 times
+#without the value for i in each secuence
+    tot = tot+1
+#so we only add +1 to tot in each secuece(5 times)
+print(tot)
+#we'll prompt 5(1+1+1+1+1)
